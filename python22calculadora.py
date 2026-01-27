2399bdcaa968ecad6e70deae77e052245168665f
“zurdopalafox” <carlos.palafox.becerril@gmail.com>
2026-01-27 20:23:44 +0100

    finfin

--- python22calculadora.py
+++ python22calculadora.py
@@ -13,21 +13,27 @@ def menu():
     print("2.Resta")
     print("3.Multiplicacion")
     print("4.Division")
+    print("5.Salir")
 
 # Programa principal
-menu()
-opcion = input("Ingrese la opcion (1/2/3/4): ")
-num1 = float(input("Ingrese el primer numero: "))
-num2 = float(input("Ingrese el segundo numero: "))
-if opcion == '1':
-    print(num1,"+",num2,"=", sumadosnumeros(num1,num2))
-elif opcion == '2':
-    print(num1,"-",num2,"=", restadosnumeros(num1,num2))
-elif opcion == '3':
-    print(num1,"*",num2,"=", multiplicaciondenumeros(num1,num2))                
-elif opcion == '4':
-    print(num1,"/",num2,"=", divisiondenumeros(num1,num2))
-else:
-    print("Opcion invalida")
+opcion = '1'
+while opcion in ['1','2','3','4','5']:   
+    menu()
+    opcion = input("Ingrese la opcion (1/2/3/4/5): ")
+    if opcion == "5":
+       print("Saliendo de la calculadora.")
+       break
+    num1 = float(input("Ingrese el primer numero: "))
+    num2 = float(input("Ingrese el segundo numero: "))
+    if opcion == '1':
+        print(num1,"+",num2,"=", sumadosnumeros(num1,num2))
+    elif opcion == '2':
+        print(num1,"-",num2,"=", restadosnumeros(num1,num2))
+    elif opcion == '3':
+        print(num1,"*",num2,"=", multiplicaciondenumeros(num1,num2))                
+    elif opcion == '4':
+        print(num1,"/",num2,"=", divisiondenumeros(num1,num2))
+
+
 
     
