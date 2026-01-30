6df46a1baaeb4c2f7d954e5f62c083473dd7511f
“zurdopalafox” <carlos.palafox.becerril@gmail.com>
2026-01-30 20:18:02 +0100

    ccccc

--- python28numeronarcisista.py
+++ python28numeronarcisista.py
@@ -1,6 +1,5 @@
+#Función para comprobar si un número es narcisista
 def narcisista(numeroentrada):
-    suma = 0
-    numero_str = str(numeroentrada)
     sumanumeros = 0
     for i in range(len(numeroentrada)):
         numeroexp = int(numeroentrada[i]) ** (len(numeroentrada))
@@ -10,13 +9,14 @@ def narcisista(numeroentrada):
     else:
        return False
 
-
+#Comprobar si un número es narcisista
 numeroentrada = input("Introduce un número : ")
 if narcisista(numeroentrada):
     print("El número es narcisista")
 else:
     print("El número no es narcisista")
 
+#Buscar números narcisistas en un rango
 rangoinferior = int(input("Introduce el rango inferior : "))
 rangosuperior = int(input("Introduce el rango superior : "))
 print("Números narcisistas entre", rangoinferior, "y", rangosuperior, " son:")
